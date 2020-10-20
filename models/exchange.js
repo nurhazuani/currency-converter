a621d6ae24024668fc1803be03f19a7bb918eb2d
Nurhazuani Nurdin <38628882+nurhazuani@users.noreply.github.com>
2020-10-20 10:04:07 +0800

    delete flag

--- models/exchange.js
+++ models/exchange.js
@@ -2,8 +2,7 @@ module.exports = (sequelize, DataTypes) => {
 	const Exchange = sequelize.define('Exchange', {
         country: DataTypes.STRING,
         code: DataTypes.STRING,
-        rate: DataTypes.DOUBLE,
-        flag: DataTypes.STRING
+        rate: DataTypes.DOUBLE
 	});
 
 	return Exchange;
